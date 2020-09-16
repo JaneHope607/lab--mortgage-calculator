a3c27cfe28b098d565a58cd1ed4803640675d035
James Monks <J.monks23@gmail.com>
2020-09-16 11:51:45 +0100

    calculation being passed down to results and been displayed

---
+++ src/components/Calculations.js
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import Result from './Result';
+
+// const Calculations = (props) => {
+    
+
+//     // const formResult = () => {
+//     //     return ()
+//     // }
+
+//     return (
+//         <>
+//         <h1>{props.salary}</h1>
+//         </>
+//     );
+// }
+
+// export default Calculations;
--- src/containers/CalculatorContainer.js
+++ src/containers/CalculatorContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Form from '../components/Form';
+import Result from '../components/Result';
 
 class CalculatorContainer extends Component {
     constructor() {
@@ -12,12 +13,14 @@ class CalculatorContainer extends Component {
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
 
-    handleFormSubmit(submittedForm) {
-        submittedForm.id = Date.now();
-        const updatedSalary = submittedForm.salary
+    handleFormSubmit(salary) {
+        salary.id = Date.now();
+        const updatedSalary = salary.salary
+        const maxHouseValue = updatedSalary * 3;
         this.setState({
-            salary: updatedSalary
-        })
+            salary: updatedSalary,
+            results: maxHouseValue  
+        });
     }
 
     render(){
@@ -25,6 +28,7 @@ class CalculatorContainer extends Component {
             <div>
                 <h2>Mortgage Calculator Container</h2>
                 <Form onFormSubmit={this.handleFormSubmit} />
+                <Result results={this.state.results}/>
             </div>
         );
     }
